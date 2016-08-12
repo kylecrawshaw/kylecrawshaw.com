3c3307dbdf7624184a23855d81443787fa476654
Kyle Crawshaw <kyle.crawshaw@gmail.com>
2016-08-12 12:43:56 -0400

    Remove deploy script

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if ([ $TRAVIS_BRANCH == "master" ])
-then
-  echo 'Deploying to s3'
-  lektor deploy s3
-  echo 'Published new changes to s3. Changes should be live at kylecrawshaw.com'
-else
-  echo "Build successful, but not publishing!"
-fi
